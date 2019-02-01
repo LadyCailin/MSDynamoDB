2391e979615146b47d61bd5caf50b8f9e3dbb340
LadyCailin <savannahcailin@gmail.com>
2019-02-01 21:52:15 +0100

    Finish making the core components work.
    Still to do, need to write documentation, and also add a cmdline tool to
    create the table in AWS.

--- src/main/java/com/methodscript/msdynamodb/DynamoDBDataSource.java
+++ src/main/java/com/methodscript/msdynamodb/DynamoDBDataSource.java
@@ -11,16 +11,19 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
-import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
 import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.laytonsmith.PureUtilities.Common.StringUtils;
 import com.laytonsmith.PureUtilities.DaemonManager;
 import com.laytonsmith.PureUtilities.Version;
@@ -34,9 +37,10 @@ import com.laytonsmith.persistence.ReadOnlyException;
 import com.laytonsmith.persistence.io.ConnectionMixinFactory;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.EnumSet;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -46,7 +50,8 @@ import java.util.Set;
 @datasource("dynamodb")
 public class DynamoDBDataSource extends AbstractDataSource {
 
-	private static final String PRIMARY_KEY_NAME = "primary";
+	private static final String PRIMARY_KEY_NAME = "key";
+	private static final String VALUE_KEY_NAME = "value";
 
 	private String protocol = "http";
 	private String host;
@@ -54,8 +59,9 @@ public class DynamoDBDataSource extends AbstractDataSource {
 	private Regions region = Regions.US_EAST_1;
 	private String accessKeyId = null;
 	private String accessKeySecret = null;
-	private AmazonDynamoDB client;
 	private String tableName;
+	private AmazonDynamoDB client;
+	private Table table;
 
 	private DynamoDBDataSource() {
 
@@ -66,34 +72,35 @@ public class DynamoDBDataSource extends AbstractDataSource {
 			throws DataSourceException {
 		super(uri, options);
 		Map<String, String> queryString = WebUtility.getQueryMap(uri.getQuery());
-		if(queryString.containsKey("protocol")) {
+		if (queryString.containsKey("protocol")) {
 			protocol = queryString.get("protocol");
 		}
 		host = uri.getHost();
-		if("aws".equalsIgnoreCase(host)) {
+		if ("aws".equalsIgnoreCase(host)) {
 			host = null;
 		}
 		port = uri.getPort();
-		if(queryString.containsKey("region")) {
+		if (queryString.containsKey("region")) {
 			region = Regions.fromName(queryString.get("region"));
 		}
-		if(queryString.containsKey("accessKeyId") && queryString.containsKey("accessKeySecret")) {
+		if (queryString.containsKey("accessKeyId") && queryString.containsKey("accessKeySecret")) {
 			accessKeyId = queryString.get("accessKeyId");
 			accessKeySecret = queryString.get("accessKeySecret");
 		}
-		if(!queryString.containsKey("tableName")) {
+		if (!queryString.containsKey("tableName")) {
 			throw new DataSourceException("tableName is a required parameter in the DynamoDB configuration.");
 		}
 		tableName = queryString.get("tableName");
+		validateTableName(tableName);
 		AmazonDynamoDBClientBuilder clientBuilder = AmazonDynamoDBClientBuilder.standard();
-		if(host != null) {
+		if (host != null) {
 			clientBuilder.withEndpointConfiguration(
 					new AwsClientBuilder.EndpointConfiguration(protocol + "://" + host + ":" + port,
 							region.name().toLowerCase()));
 		} else {
 			clientBuilder.withRegion(region);
 		}
-		if(accessKeyId != null) {
+		if (accessKeyId != null) {
 			clientBuilder.setCredentials(new AWSCredentialsProvider() {
 				@Override
 				public AWSCredentials getCredentials() {
@@ -111,7 +118,8 @@ public class DynamoDBDataSource extends AbstractDataSource {
 				}
 
 				@Override
-				public void refresh() {}
+				public void refresh() {
+				}
 			});
 		}
 		client = clientBuilder.build();
@@ -119,7 +127,7 @@ public class DynamoDBDataSource extends AbstractDataSource {
 			client.describeTable(tableName).getTable();
 		} catch (ResourceNotFoundException e) {
 			// Need to create a table with this name
-			if(host == null) {
+			if (host == null) {
 				throw new DataSourceException("The table \"" + tableName + "\" was not found in "
 						+ (host == null ? "AWS:" + region.getName() : host)
 						+ ". You must manually create this table yourself. Please see the documentation for details"
@@ -128,50 +136,88 @@ public class DynamoDBDataSource extends AbstractDataSource {
 				createTable(tableName, client);
 			}
 		}
+		table = new DynamoDB(client).getTable(tableName);
+	}
+
+	private void validateTableName(String tableName) throws DataSourceException {
+		List<String> errors = new ArrayList<>();
+		if (tableName.length() < 3 || tableName.length() > 255) {
+			errors.add("Table name length must be between 3 and 255 characters.");
+		}
+		if (!tableName.matches("[a-zA-Z0-9_\\-\\.]+")) {
+			errors.add("Table names may only contain a-z, A-Z, 0-9, -, ., _");
+		}
+		if (!errors.isEmpty()) {
+			throw new DataSourceException("There " + StringUtils.PluralHelper(errors.size(), "error") + " with the"
+					+ " the provided table name \"" + tableName + "\":\n- " + StringUtils.Join(errors, "\n- "));
+		}
 	}
 
 	private static void createTable(String tableName, AmazonDynamoDB client) {
 		// THIS SHOULD ONLY BE USED LOCALLY
-		client.createTable(new CreateTableRequest(tableName,
-			Arrays.asList(new KeySchemaElement(PRIMARY_KEY_NAME, KeyType.HASH).withAttributeName(PRIMARY_KEY_NAME)))
-				.withAttributeDefinitions(new AttributeDefinition(PRIMARY_KEY_NAME, ScalarAttributeType.S))
-				.withProvisionedThroughput(new ProvisionedThroughput(1000L, 1000L)));
+		DynamoDB dynamoDB = new DynamoDB(client);
+
+		List<AttributeDefinition> attributeDefinitions = new ArrayList<>();
+		attributeDefinitions.add(new AttributeDefinition().withAttributeName(PRIMARY_KEY_NAME)
+				.withAttributeType(ScalarAttributeType.S));
+
+		List<KeySchemaElement> keySchema = new ArrayList<>();
+		keySchema.add(new KeySchemaElement().withAttributeName(PRIMARY_KEY_NAME).withKeyType(KeyType.HASH));
+
+		CreateTableRequest request = new CreateTableRequest()
+				.withTableName(tableName)
+				.withKeySchema(keySchema)
+				.withAttributeDefinitions(attributeDefinitions)
+				.withProvisionedThroughput(new ProvisionedThroughput()
+						.withReadCapacityUnits(1000L)
+						.withWriteCapacityUnits(1000L));
+
+		Table table = dynamoDB.createTable(request);
+		try {
+			table.waitForActive();
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
 	}
 
 	@Override
 	protected void startTransaction0(DaemonManager dm) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		// TODO: We can create a batch mode with this, but it still has to be paginated, because batches can only
+		// be up to 25 items, due to limitations in AWS.
 	}
 
 	@Override
 	protected void stopTransaction0(DaemonManager dm, boolean rollback) throws DataSourceException, IOException {
-		throw new UnsupportedOperationException("Not supported yet.");
+
 	}
 
 	@Override
 	protected boolean set0(DaemonManager dm, String[] key, String value) throws ReadOnlyException,
 			DataSourceException, IOException {
-		Map<String, AttributeValue> map = new HashMap<>();
-		map.put(PRIMARY_KEY_NAME, new AttributeValue(StringUtils.Join(key, ".")).withS(value));
-		client.putItem(new PutItemRequest(tableName, map));
+		Item item = new Item()
+				.withPrimaryKey(PRIMARY_KEY_NAME, StringUtils.Join(key, "."))
+				.withString(VALUE_KEY_NAME, value);
+		table.putItem(item);
 		return true;
 	}
 
 	@Override
 	protected String get0(String[] key) throws DataSourceException {
-		Map<String, AttributeValue> map = new HashMap<>();
-		map.put(PRIMARY_KEY_NAME, new AttributeValue(StringUtils.Join(key, ".")));
-		Map<String, AttributeValue> ret = client.getItem(new GetItemRequest(tableName, map)).getItem();
-		if(ret == null) {
+		Item item = table.getItem(PRIMARY_KEY_NAME, StringUtils.Join(key, "."));
+		if (item == null) {
 			return null;
-		} else {
-			return ret.get(StringUtils.Join(key, ".")).getS();
 		}
+		return item.getString(VALUE_KEY_NAME);
 	}
 
 	@Override
 	public Set<String[]> keySet(String[] keyBase) throws DataSourceException {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		ScanResult res = client.scan(new ScanRequest(tableName));
+		Set<String[]> ret = new HashSet<>();
+		for (Map<String, AttributeValue> r : res.getItems()) {
+			ret.add(r.get(PRIMARY_KEY_NAME).getS().split("\\."));
+		}
+		return ret;
 	}
 
 	@Override
@@ -191,7 +237,8 @@ public class DynamoDBDataSource extends AbstractDataSource {
 
 	@Override
 	public void disconnect() throws DataSourceException {
-
+		client = null;
+		table = null;
 	}
 
 	@Override
