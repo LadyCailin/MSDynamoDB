a58d93634e169ac315e39ca340df2a28e90564d0
LadyCailin <savannahcailin@gmail.com>
2019-02-11 16:09:10 +0100

    Properly register the extension via the MSExtension annotation.

--- src/main/java/com/methodscript/msdynamodb/MSDynamoDB.java
+++ src/main/java/com/methodscript/msdynamodb/MSDynamoDB.java
@@ -8,10 +8,12 @@ package com.methodscript.msdynamodb;
 import com.laytonsmith.PureUtilities.SimpleVersion;
 import com.laytonsmith.PureUtilities.Version;
 import com.laytonsmith.core.extensions.AbstractExtension;
+import com.laytonsmith.core.extensions.MSExtension;
 
 /**
  *
  */
+@MSExtension("MSDynamoDB")
 public class MSDynamoDB extends AbstractExtension {
 
 	@Override
